14724d3686a460770021ca2118ca1dbcde717a4a
myalanon <s.ghubmyalanon@k5pta.com>
2026-07-31 15:41:31 -0500

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -6,12 +6,26 @@
 // document.getElementById("demo").innerHTML=
 
 
-function ctac() 
+function ctacold() 
 {
-    MYAT = '@';
-    MYNEXT1 = 'i';
-    MYPD='.';
-    MYNEXT2='c';
-    zz= 'mailto:' + String.fromCharCode(parseInt(0x49, 16))+'f'+'a'+'l'+'a'+'n'+'o'+'n'+ MYAT +'g'+'m'+'a'+ MYNEXT1+'l'+ MYPD + MYNEXT2 +'o'+'m';
-    window.location = zz;
+//   MYAT = '@';
+//    MYNEXT1 = 'i';
+//    MYPD='.';
+//    MYNEXT2='c';
+//    zz= 'mailto:' + String.fromCharCode(parseInt(0x49, 16))+'f'+'a'+'l'+'a'+'n'+'o'+'n'+ MYAT +'g'+'m'+'a'+ MYNEXT1+'l'+ MYPD + MYNEXT2 +'o'+'m';
+    zzz = "mailto:steve@example.com";
+    window.location.href = zzz;
+}
+
+
+function ctac() {
+    const mailtoUrl = "mailto:sfalanon@gmail.com";
+    
+    const link = document.createElement('a');
+    link.href = mailtoUrl;
+    link.style.display = 'none';
+    
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link); // Clean up the DOM
 }
