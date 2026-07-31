c27a5671204123facfb944bb1ac521c7d4858ce2
myalanon <s.ghubmyalanon@k5pta.com>
2026-07-31 12:52:12 -0500

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -1,12 +1,17 @@
 
 
+
 // <div id="demo" style="white-space: pre-wrap;"></div>
 // '\u00A9' is copyribht char
+// document.getElementById("demo").innerHTML=
 
 
-MYAT = '@';
-MYNEXT1 = 'i';
-MYPD='.';
-MYNEXT2='c';
-document.getElementById("demo").innerHTML=
-String.fromCharCode(parseInt(0x49, 16))+'f'+'a'+'l'+'a'+'n'+'o'+'n'+ MYAT +'g'+'m'+'a'+ MYNEXT1+'l'+ MYPD + MYNEXT2 +'o'+'m';
+function ctac() 
+{
+    MYAT = '\u0040';
+    MYNEXT1 = 'i';
+    MYPD='\u002E';
+    MYNEXT2='c';
+    zz= 'mailto:' + String.fromCharCode(parseInt(0x49, 16))+'f'+'a'+'l'+'a'+'n'+'o'+'n'+ MYAT +'g'+'m'+'a'+ MYNEXT1+'l'+ MYPD + MYNEXT2 +'o'+'m';
+    window.location = zz;
+}
