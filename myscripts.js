1df3b7f990f0ea086f2970eaa62f2cc156363eb5
myalanon <s.ghubmyalanon@k5pta.com>
2026-07-31 23:35:25 -0500

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,15 @@
 
-MYAT = '@';
-MYNEXT1 = 'i';
-MYPD='.';
-MYNEXT2='c';
-zz=String.fromCharCode(parseInt(0x49, 16))+'f'+'a'+'l'+'a'+'n'+'o'+'n'+ MYPD + 'webmaster' + MYAT +'g'+'m'+'a'+ MYNEXT1+'l'+ MYPD + MYNEXT2 +'o'+'m';
-document.getElementById("demo").innerHTML= " Please send ema" + MYNEXT1 + "l to: " + zz;
+const MYAT = '@';
+const MYNEXT1 = 'i';
+const MYPD='.';
+const MYNEXT2='c';
+const zz=String.fromCharCode(parseInt(0x49, 16))+'f'+'a'+'l'+'a'+'n'+'o'+'n'+ MYPD + 'webmaster' + MYAT +'g'+'m'+'a'+ MYNEXT1+'l'+ MYPD + MYNEXT2 +'o'+'m';
+
+  
+// Update the link attributes when the DOM loads
+const link = document.getElementById("email-link");
+link.href = "ma" + MYNEXT1 + "lto:" + zz;
+// link.innerText = fullEmail; // Optional: Changes 'Contact Us' to the email text
+
+
+
